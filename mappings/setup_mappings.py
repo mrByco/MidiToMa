bb06de3fc72576fbc0fa319e4f1027a7be644c0a
Mátyás <matyi0918@gmail.com>
2021-07-01 14:39:01 +0200

    Playback buttons work

--- mappings/setup_mappings.py
+++ mappings/setup_mappings.py
@@ -1,9 +1,13 @@
+from converters.fix_value_filter import FixValueFilter
+from converters.value_go_trough import ValueGoTrough
 from midi_controller import MidiController
 from translator import Translator, FaderTranslator
 
 
 def setup(midi: MidiController):
     setup_main_exec_faders(midi)
+    setup_main_exec_buttons(midi)
+    setup_5x9_buttons(midi)
     pass
 
 
@@ -18,5 +22,37 @@ def setup_main_exec_faders(midi: MidiController):
         FaderTranslator((182, 7), (157, 7)),
         FaderTranslator((183, 7), (158, 7)),
         FaderTranslator((176, 14), (159, 7)),
+    ]
+    midi.apc_ma_translators.extend(translators)
+
+
+def setup_main_exec_buttons(midi: MidiController):
+    translators: [Translator] = [
+        Translator((ValueGoTrough(), FixValueFilter(50, 50), FixValueFilter(127, 127))),
+        Translator((ValueGoTrough(), FixValueFilter(49, 49), FixValueFilter(127, 127))),
+        Translator((ValueGoTrough(), FixValueFilter(48, 48), FixValueFilter(127, 127))),
+    ]
+    midi.apc_ma_translators.extend(translators)
+
+
+def setup_5x9_buttons(midi: MidiController):
+    translators: [Translator] = [
+        Translator((ValueGoTrough(), FixValueFilter(51, 51), FixValueFilter(127, 127))),
+        Translator((ValueGoTrough(), FixValueFilter(52, 52), FixValueFilter(127, 127))),
+
+        Translator((ValueGoTrough(), FixValueFilter(53, 53), FixValueFilter(127, 127))),
+        Translator((ValueGoTrough(), FixValueFilter(54, 54), FixValueFilter(127, 127))),
+        Translator((ValueGoTrough(), FixValueFilter(55, 55), FixValueFilter(127, 127))),
+        Translator((ValueGoTrough(), FixValueFilter(56, 56), FixValueFilter(127, 127))),
+        Translator((ValueGoTrough(), FixValueFilter(57, 57), FixValueFilter(127, 127))),
+        Translator((ValueGoTrough(), FixValueFilter(58, 58), FixValueFilter(127, 127))),
+
+        Translator((ValueGoTrough(), FixValueFilter(80, 80), FixValueFilter(127, 127))),
+        Translator((ValueGoTrough(), FixValueFilter(81, 81), FixValueFilter(127, 127))),
+        Translator((ValueGoTrough(), FixValueFilter(82, 82), FixValueFilter(127, 127))),
+        Translator((ValueGoTrough(), FixValueFilter(83, 83), FixValueFilter(127, 127))),
+        Translator((ValueGoTrough(), FixValueFilter(84, 84), FixValueFilter(127, 127))),
+        Translator((ValueGoTrough(), FixValueFilter(85, 85), FixValueFilter(127, 127))),
+        Translator((ValueGoTrough(), FixValueFilter(86, 86), FixValueFilter(127, 127))),
     ]
     midi.apc_ma_translators.extend(translators)
